4257822739a29c931041e52773fcb3be58b9b28a
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2020-08-08 01:19:00 +0530

    Sorted array with square IB Leetcode

---
+++ array/sortArraySquares.cpp
@@ -0,0 +1,16 @@
+// Sort Array With Squares! Leetcode + IB 
+vector<int> Solution::solve(vector<int> &A) {
+    int n = A.size();
+        vector<int> result(n);
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (abs(A[i]) > abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+}
