2671d855caf2c1c9bbc86345228dde15abaef200
simranpreet kaur <simranpreetkaurkhalsa13@gmail.com>
2020-08-29 17:48:07 +0530

    Create max_subarray_dp.cpp

---
+++ max_subarray_dp.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+     int n = nums.size();
+     vector<int> dp(n,0);
+     dp[0]=nums[0];
+     int maxi=dp[0];
+     for(int i=1;i<n;i++) {
+         if(dp[i-1]>0) {
+             dp[i]=nums[i]+dp[i-1];
+         } else {
+             dp[i]=nums[i];
+         }
+      //   cout<<dp[i]<<" ";
+         maxi=max(maxi,dp[i]);
+     }
+        return maxi;
+    }
+    
+};
