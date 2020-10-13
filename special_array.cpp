80a1b2ca10c31524f9a642e30aaedf7f0dc733dc
simranpreet kaur <simranpreetkaurkhalsa13@gmail.com>
2020-10-13 07:42:02 +0530

    Create special_array.cpp

---
+++ special_array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int res=-1;
+        int c=0;
+        for(int i=nums.size();i>0;i--) {
+            c=0;
+            for(int j=0;j<nums.size();j++) {
+                if(nums[j]>=i) {
+                    c++;
+                }
+            }
+            if(c==i) {
+                res=i;
+            }
+        }
+        return res;
+    }
+};
